bdcd5c311b0764e3a0d865da7cc60bdda9af7bf1
jsongalvez <jsgalvez@mymail.mapua.edu.ph>
2026-02-07 00:51:23 +0800

    configure ktlint and detekt linting
    - Add ktlint and detekt Gradle plugins
    - Add .editorconfig for code style
    - Add detekt.yml configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,24 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.8"
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    ktlint {
+        version.set("1.8.0")
+        outputToConsole.set(true)
+        ignoreFailures.set(false)
+    }
+
+    detekt {
+        buildUponDefaultConfig = true
+        allRules = false
+        config.setFrom(files("$rootDir/config/detekt/detekt.yml"))
+    }
 }
