88e0a4cbeaa42e22c69465aaeb24ca0007be7e49
jsongalvez <jsgalvez@mymail.mapua.edu.ph>
2026-03-20 14:31:44 +0800

    replace HTTP status utility with Javalin HTTP status implementation
    Signed-off-by: jsongalvez <jsgalvez@mymail.mapua.edu.ph>

--- backend/src/main/kotlin/com/companyb/companyapp/api/routes/AuthRoutes.kt
+++ backend/src/main/kotlin/com/companyb/companyapp/api/routes/AuthRoutes.kt
@@ -3,7 +3,6 @@ package com.companyb.companyapp.api.routes
 import com.companyb.companyapp.dto.LoginRequest
 import com.companyb.companyapp.dto.LoginResponse
 import com.companyb.companyapp.service.AuthService
-import com.companyb.companyapp.utils.HTTP
 import io.javalin.config.JavalinConfig
 import io.javalin.http.bodyAsClass
 
@@ -13,9 +12,9 @@ object AuthRoutes {
             val loginRequest = context.bodyAsClass<LoginRequest>()
             val loginResponse: LoginResponse = AuthService.login(loginRequest.username, loginRequest.password)
             if (loginResponse.token.isNullOrBlank()) {
-                context.status(HTTP.Response.ClientError.UNAUTHORIZED)
+                context.status(io.javalin.http.HttpStatus.UNAUTHORIZED)
             } else {
-                context.status(HTTP.Response.Successful.OK)
+                context.status(io.javalin.http.HttpStatus.OK)
                 context.result(loginResponse.token.toString())
             }
         }
