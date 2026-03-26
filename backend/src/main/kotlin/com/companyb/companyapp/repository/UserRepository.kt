24e2e6680cbc9c1197adab8824bf2f84690400b4
jsongalvez <jsgalvez@mymail.mapua.edu.ph>
2026-03-26 23:49:35 +0800

    add logs for UserRepository
    Signed-off-by: jsongalvez <jsgalvez@mymail.mapua.edu.ph>

--- backend/src/main/kotlin/com/companyb/companyapp/repository/UserRepository.kt
+++ backend/src/main/kotlin/com/companyb/companyapp/repository/UserRepository.kt
@@ -2,9 +2,12 @@ package com.companyb.companyapp.repository
 
 import com.companyb.companyapp.repository.model.AppUser
 import com.companyb.companyapp.repository.model.AppUserTable
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
+
+private val logger = KotlinLogging.logger { }
 
 object UserRepository {
     fun findByUsername(username: String): AppUser? =
@@ -19,7 +22,7 @@ object UserRepository {
                         passwordHash = row[AppUserTable.passwordHash],
                     )
                 }.singleOrNull()
-        }
+        }.also { logger.info { "[FIND-BY-USERNAME] Fetched username" } }
 
     fun createUser(
         username: String,
@@ -31,6 +34,6 @@ object UserRepository {
                     it[AppUserTable.username] = username
                     it[AppUserTable.passwordHash] = passwordHash
                 }
-        }
+        }.also { logger.info { "[CREATE-USER] Added user to ${AppUserTable.tableName} table" } }
     }
 }
