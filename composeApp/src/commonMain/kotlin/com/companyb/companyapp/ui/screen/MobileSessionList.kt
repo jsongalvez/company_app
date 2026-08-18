fb7574315100ca87f82e28f688ae795495fa2c2d
Jayson Galvez <jaysonsullanogalvez@gmail.com>
2026-08-19 04:12:59 +0800

    chore(compose): remove unused mobile import

--- composeApp/src/commonMain/kotlin/com/companyb/companyapp/ui/screen/MobileSessionList.kt
+++ composeApp/src/commonMain/kotlin/com/companyb/companyapp/ui/screen/MobileSessionList.kt
@@ -1,6 +1,5 @@
 package com.companyb.companyapp.ui.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
